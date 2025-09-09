1ae82fd3f24b32f4eaadc96d24d3d1484bbb47d1
seichaslucas-byte <seichas.lucas@gmail.com>
2025-09-09 19:24:11 +0000

    PIB

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 int main(){
-    int populacao, PontosTuristicos;
-    float Area, Pib;
+    int idade, matricula;
+    float PIB;
+    char nome[50];
     
+    
+    printf("Carta 1\n");
+    printf("Estado:");
+    scanf("%s", &nome);
+    
+    printf("PIB:");
+    scanf("%f", &PIB);
+    
+
 
 
     return 0;
