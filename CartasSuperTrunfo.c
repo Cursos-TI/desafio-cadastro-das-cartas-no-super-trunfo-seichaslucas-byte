f7faecfc7c5301b37b2b6d3a1e276ec455bf5946
seichaslucas-byte <seichas.lucas@gmail.com>
2025-09-09 19:41:35 +0000

    Novo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 int main(){
     int idade, matricula;
-    float PIB;
+    float PIB, Area;
     char nome[50];
     
     
-    printf("Carta 1\n");
+    printf("Desafio Jogo Trunfo\n Carta 1\n");
     printf("Estado:");
     scanf("%s", &nome);
     
     printf("PIB:");
     scanf("%f", &PIB);
+
+    printf("Area:");
+    scanf("%f", &Area);
+
+
     
 
 
